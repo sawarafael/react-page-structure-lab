30f3348fc6e3a7a292cdc91e5e5380346dedf1f2
Rafael Soares <rafael.soares@sigtrans.com.br>
2021-11-29 20:06:41 -0300

    Third Commit

--- src/Containers/Appbar/Appsidebar.tsx
+++ src/Containers/Appbar/Appsidebar.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
-import { AppBar, Drawer, Toolbar } from "@material-ui/core";
+import { AppBar, Drawer, IconButton, Toolbar } from "@material-ui/core";
+import MenuIcon from '@mui/icons-material/Menu';
 
 export const AppSideBar = ({ children }: any) => {
   return (
     <>
-      <AppBar>
-        <Toolbar>Lab de Repositório Uranus com Autenticação IS</Toolbar>
+      <AppBar color="inherit">
+        <Toolbar disableGutters={true} variant="dense" >
+            <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+        </Toolbar>
       </AppBar>
       <Drawer>Sidebar</Drawer>
       {children}
